0ba74bd5681f83436e96860e893fa315cd0bc577
sungwon cho <josw@sungwon-ui-MacBook-Air.local>
2014-11-25 00:52:35 +0900

    config

--- src/main/java/com/swj/stock/configuration/PersistenceConfig.java
+++ src/main/java/com/swj/stock/configuration/PersistenceConfig.java
@@ -2,6 +2,8 @@ package com.swj.stock.configuration;
 
 import java.util.Properties;
 
+import javax.persistence.EntityManagerFactory;
+
 import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,19 +11,25 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.instrument.classloading.InstrumentationLoadTimeWeaver;
 import org.springframework.orm.hibernate4.HibernateTransactionManager;
 import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.swj.stock.data.db;
 
 @Configuration
 @EnableTransactionManagement
-@ComponentScan(basePackageClasses = db.class)
+@EnableJpaRepositories(basePackageClasses=db.class)
 public class PersistenceConfig {
 
 	@Bean
-	public BasicDataSource getDataSource() {
+	public BasicDataSource dataSource() {
 		BasicDataSource dataSource = new BasicDataSource();
 		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
 		dataSource.setUrl("jdbc:mysql://localhost:3306");
@@ -30,32 +38,71 @@ public class PersistenceConfig {
 
 		return dataSource;
 	}
+	
+	 @Bean
+	  public EntityManagerFactory entityManagerFactory() {
 
-	@Bean
-	public LocalSessionFactoryBean sessionFactory() {
-		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-		sessionFactory.setDataSource(getDataSource());
-		sessionFactory
-				.setPackagesToScan(new String[] { "org.baeldung.spring.persistence.model" });
-		sessionFactory.setHibernateProperties(hibernateProperties());
+	    HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+	    vendorAdapter.setGenerateDdl(true);
 
-		return sessionFactory;
-	}
+	    LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+	    factory.setJpaVendorAdapter(vendorAdapter);
+	    factory.setPackagesToScan("com.swj.stock");
+	    factory.setDataSource(dataSource());
+	    factory.setJpaProperties(hibernateProperties());
+	    factory.afterPropertiesSet();
 
-	@Bean
-	@Autowired
-	public HibernateTransactionManager transactionManager(
-			SessionFactory sessionFactory) {
-		HibernateTransactionManager txManager = new HibernateTransactionManager();
-		txManager.setSessionFactory(sessionFactory);
+	    return factory.getObject();
+	  }
 
-		return txManager;
-	}
+	  @Bean
+	  public PlatformTransactionManager transactionManager() {
 
-	@Bean
-	public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
-		return new PersistenceExceptionTranslationPostProcessor();
-	}
+	    JpaTransactionManager txManager = new JpaTransactionManager();
+	    txManager.setEntityManagerFactory(entityManagerFactory());
+	    return txManager;
+	  }
+	
+//	@Bean
+//	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+//		LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+//		HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+//		vendorAdapter.setGenerateDdl(Boolean.TRUE);
+//		vendorAdapter.setShowSql(Boolean.TRUE);
+//		factory.setDataSource(dataSource());
+//		factory.setJpaVendorAdapter(vendorAdapter);
+//		factory.setJpaProperties(hibernateProperties());
+//		factory.afterPropertiesSet();
+//		factory.setPackagesToScan("com.swj.stock.data");
+//		factory.setLoadTimeWeaver(new InstrumentationLoadTimeWeaver());
+//		return factory;
+//	}
+////
+////	@Bean
+////	public LocalSessionFactoryBean sessionFactory() {
+////		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+////		sessionFactory.setDataSource(getDataSource());
+////		sessionFactory
+////				.setPackagesToScan(new String[] { "org.baeldung.spring.persistence.model" });
+////		sessionFactory.setHibernateProperties(hibernateProperties());
+////
+////		return sessionFactory;
+////	}
+//
+//	@Bean
+//	@Autowired
+//	public HibernateTransactionManager transactionManager(
+//			SessionFactory sessionFactory) {
+//		HibernateTransactionManager txManager = new HibernateTransactionManager();
+//		txManager.setSessionFactory(sessionFactory);
+//
+//		return txManager;
+//	}
+//
+//	@Bean
+//	public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
+//		return new PersistenceExceptionTranslationPostProcessor();
+//	}
 
 	Properties hibernateProperties() {
 		return new Properties() {
