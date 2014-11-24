a667b77722e243cedc17d65df637fa0b7fa32287
sungwon cho <josw@sungwon-ui-MacBook-Air.local>
2014-11-24 23:11:50 +0900

    config

--- src/main/java/com/swj/stock/configuration/PersistenceConfig.java
+++ src/main/java/com/swj/stock/configuration/PersistenceConfig.java
@@ -1,6 +1,5 @@
 package com.swj.stock.configuration;
 
-
 import java.util.Properties;
 
 import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
@@ -18,54 +17,55 @@ import com.swj.stock.data.db;
 
 @Configuration
 @EnableTransactionManagement
-@ComponentScan(basePackageClasses=db.class)
+@ComponentScan(basePackageClasses = db.class)
 public class PersistenceConfig {
-	
+
 	@Bean
 	public BasicDataSource getDataSource() {
 		BasicDataSource dataSource = new BasicDataSource();
 		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
 		dataSource.setUrl("jdbc:mysql://localhost:3306");
 		dataSource.setUsername("root");
-//		dataSource.setPassword("pass");
-		
+		// dataSource.setPassword("pass");
+
 		return dataSource;
 	}
-	
-	 @Bean
-	   public LocalSessionFactoryBean sessionFactory() {
-	      LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-	      sessionFactory.setDataSource(getDataSource());
-	      sessionFactory.setPackagesToScan(new String[] { "org.baeldung.spring.persistence.model" });
-	      sessionFactory.setHibernateProperties(hibernateProperties());
-	 
-	      return sessionFactory;
-	   }
-	
-	@Bean
-	   @Autowired
-	   public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
-	      HibernateTransactionManager txManager = new HibernateTransactionManager();
-	      txManager.setSessionFactory(sessionFactory);
-	 
-	      return txManager;
-	   }
-	 
-	   @Bean
-	   public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
-	      return new PersistenceExceptionTranslationPostProcessor();
-	   }
-	 
-	   Properties hibernateProperties() {
-	      return new Properties() {
-	         {
-	            setProperty("hibernate.hbm2ddl.auto", "create-drop");
-	            setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
-	            setProperty("hibernate.globally_quoted_identifiers", "true");
-	         }
-	      };
-	   } 
 
-	
+	@Bean
+	public LocalSessionFactoryBean sessionFactory() {
+		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+		sessionFactory.setDataSource(getDataSource());
+		sessionFactory
+				.setPackagesToScan(new String[] { "org.baeldung.spring.persistence.model" });
+		sessionFactory.setHibernateProperties(hibernateProperties());
+
+		return sessionFactory;
+	}
+
+	@Bean
+	@Autowired
+	public HibernateTransactionManager transactionManager(
+			SessionFactory sessionFactory) {
+		HibernateTransactionManager txManager = new HibernateTransactionManager();
+		txManager.setSessionFactory(sessionFactory);
+
+		return txManager;
+	}
+
+	@Bean
+	public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
+		return new PersistenceExceptionTranslationPostProcessor();
+	}
+
+	Properties hibernateProperties() {
+		return new Properties() {
+			{
+				setProperty("hibernate.hbm2ddl.auto", "create-drop");
+				setProperty("hibernate.dialect",
+						"org.hibernate.dialect.MySQL5Dialect");
+				setProperty("hibernate.globally_quoted_identifiers", "true");
+			}
+		};
+	}
 
 }
